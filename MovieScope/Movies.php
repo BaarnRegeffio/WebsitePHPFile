19d56c2c603f5a6a259ea7384e26123f5b7f129b
BaarnRegeffio <162223435+BaarnRegeffio@users.noreply.github.com>
2024-03-26 21:21:49 -0300

    Add files via upload

---
+++ MovieScope/Movies.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>FilmScope</title>
+    <link rel="stylesheet" href="Movies.css" />
+    <!--Google Fonts Icons-->
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Roboto:ital,wght@0,400;1,500&display=swap"
+      rel="stylesheet"
+    />
+  </head>
+
+  <body>
+    <header class="Mega">
+      <nav class="navbar">
+        <a href="Index1.html" class="logo">FilmScope<span></span></a>
+        <hr>
+        <ul class="menu-links">
+          
+          <div class="dropdown"><li><a href="#">Genre</a></li>
+            <div class="dropdown-content">
+              <a href="#">Action</a>
+              <a href="#">Horror</a>
+              <a href="#">Fantasy</a>
+              <a href="#">Comedy</a>
+              <a href="#">Drama</a>
+            </div>
+          </div>
+
+          <li><a class="men" href="#">About us</a></li>
+          <li><a class="men"    href="Login.html">Login</a></li>
+          
+        </ul>
+      </nav>
+
+
+    
+
+
+
+    </header>
+  
+    <div class="banner">
+
+    </div>
+  
+
+
+<!-- Footer -->
+<div class="Popular">
+  <h3>Popular List</h3>
+  <a href="#"><img class="popular-movie" src="MoviePosters/The Batman (1).jpg"  alt="The Batman"></a>
+  <img class="popular-movie" src="MoviePosters/Avengers.jpg"  alt="Avengers">
+  <img class="popular-movie" src="MoviePosters/Marvel's Black Panther Wakanda Forever Review.jpg"  alt="Black Panther">
+  <img class="popular-movie" src="MoviePosters/No Time To Die Full Movie Free Online.jpg"  alt="No Time To Die Full Movie">
+  <img class="popular-movie" src="MoviePosters/Interstellar.jpg"  alt="Interstellar">
+  <img class="popular-movie" src="MoviePosters/Oppenheimer (2023).jpg" alt="Oppenheimer">
+   <button class="WatchNow">WatchNow</button>
+</div>
+
+
+
+
+
+
+
+  </body>
+</html>
