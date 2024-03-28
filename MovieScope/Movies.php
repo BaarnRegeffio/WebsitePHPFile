7c7c79f2af286541d6782494a756eb87b31bd87d
BaarnRegeffio <162223435+BaarnRegeffio@users.noreply.github.com>
2024-03-28 14:31:17 -0300

    Update Movies.php

--- MovieScope/Movies.php
+++ MovieScope/Movies.php
@@ -4,6 +4,11 @@
     <!-- Required meta tags -->
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" 
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+
+
     <title>FilmScope</title>
     <link rel="stylesheet" href="Movies.css" />
     <!--Google Fonts Icons-->
@@ -45,21 +50,33 @@
     </header>
   
     <div class="banner">
-
+ 
+      <div class="Searchbar">
+      <input type="text" placeholder="Search . ." name="Search">
+      <div class="Search-container">
+      <button type="submit">Search</i></button>
     </div>
-  
+    
+   <div class="Icons">
+        <i class="fa-solid fa-magnifying-glass"></i>
+      </div>
+    </div>
+    </div>
+   
+    
 
 
-<!-- Footer -->
+<!-- MovieList -->
 <div class="Popular">
   <h3>Popular List</h3>
-  <a href="#"><img class="popular-movie" src="MoviePosters/The Batman (1).jpg"  alt="The Batman"></a>
+ <a href="#"><img class="popular-movie" src="MoviePosters/The Batman (1).jpg"  alt="The Batman"></a>
   <img class="popular-movie" src="MoviePosters/Avengers.jpg"  alt="Avengers">
   <img class="popular-movie" src="MoviePosters/Marvel's Black Panther Wakanda Forever Review.jpg"  alt="Black Panther">
   <img class="popular-movie" src="MoviePosters/No Time To Die Full Movie Free Online.jpg"  alt="No Time To Die Full Movie">
   <img class="popular-movie" src="MoviePosters/Interstellar.jpg"  alt="Interstellar">
   <img class="popular-movie" src="MoviePosters/Oppenheimer (2023).jpg" alt="Oppenheimer">
-   <button class="WatchNow">WatchNow</button>
+   
+
 </div>
 
 
